d9f18b8d2387b94f6dce02d91470a7e8cd640852
Surendra Shrestha <surendra.shrestha@asteroidventure.com>
2021-01-18 09:31:51 +0545

    optimize styles

--- NepseClient.Commons/Contracts/IMeroShareConfiguration.cs
+++ NepseClient.Commons/Contracts/IMeroShareConfiguration.cs
@@ -5,7 +5,6 @@
         string Username { get; set; }
         string Password { get; set; }
         string ClientId { get; set; }
-        string[] Demat { get; set; }
         bool RememberPassword { get; set; }
         string AuthHeader { get; set; }
 
